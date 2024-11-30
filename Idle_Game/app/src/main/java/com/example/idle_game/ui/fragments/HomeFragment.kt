499c49bfd8ee81a453cf0d7ca41fa3a96a3e1826
Alex <alex.khr@yahoo.com>
2024-11-30 22:13:18 +0100

    Implementation counter that incremented after one of the producers were clicked

--- Idle_Game/app/src/main/java/com/example/idle_game/ui/fragments/HomeFragment.kt
+++ Idle_Game/app/src/main/java/com/example/idle_game/ui/fragments/HomeFragment.kt
@@ -6,35 +6,45 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.idle_game.R
 import com.example.idle_game.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+    private var counter = 0
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val binding = FragmentHomeBinding.inflate(inflater, container, false)
 
         val buttonHacker = binding.root.findViewById<Button>(R.id.buttonHacker)
         val buttonBotNet = binding.root.findViewById<Button>(R.id.buttonBotnet)
-        val buttonCryptoMiner = binding.root.findViewById<Button>(R.id.buttonCryptoMiner)
+        val buttonMiner = binding.root.findViewById<Button>(R.id.buttonMiner)
+        val counterText = binding.counterText
 
         buttonHacker.setOnClickListener {
-            Log.e("Hacker", "Hacker in use")
+            counter++
+            updateCounter(counterText)
         }
 
         buttonBotNet.setOnClickListener {
-            Log.e("Bot-Net", "Bot-Net in use")
+            counter+=2
+            updateCounter(counterText)
         }
 
-        buttonCryptoMiner.setOnClickListener {
-            Log.e("Crypto Miner", "Miner in use")
+        buttonMiner.setOnClickListener {
+            counter+=3
+            updateCounter(counterText)
         }
 
         return binding.root
+    }
+
+    private fun updateCounter(counterText: TextView) {
+        counterText.text = "Counter: $counter"
     }
 }
