5cbf858dc87190fe55d0d70fc2467973fce5dc2c
joris-debug <jorisludwig28@gmail.com>
2024-12-03 20:27:03 +0100

    Added new upgrade methods in the rep

--- Idle_Game/app/src/main/java/com/example/idle_game/data/repositories/GameRepository.kt
+++ Idle_Game/app/src/main/java/com/example/idle_game/data/repositories/GameRepository.kt
@@ -29,7 +29,7 @@ class GameRepository(
         try {
             val resp = api.signUp(signUpRequest)
             val refreshToken = sharedPreferences.getString("refresh_token", null)
-            if(refreshToken != null) {
+            if (refreshToken != null) {
                 val playerData = PlayerData(
                     username = username,
                     password = password,
@@ -49,7 +49,7 @@ class GameRepository(
         try {
             val resp = api.login(playerData.refreshToken)
             val accessToken = sharedPreferences.getString("access_token", null)
-            if(accessToken != null) {
+            if (accessToken != null) {
                 gameDao.updateAccessToken(accessToken)
             }
         } catch (e: HttpException) {
@@ -61,11 +61,11 @@ class GameRepository(
     suspend fun updateShop(onFailure: () -> Unit = {}) {
         val playerData = playerDataFlow.first()
         try {
-            if(playerData.accessToken == null) {
+            if (playerData.accessToken == null) {
                 throw NullPointerException("accessToken can't be null")
             }
             val resp = api.getItems(playerData.accessToken)
-            for(item in resp) {
+            for (item in resp) {
                 gameDao.insertShop(item.toShopData())
             }
         } catch (e: HttpException) {
@@ -83,55 +83,139 @@ class GameRepository(
         gameDao.updateBitcoins(bitcoins)
     }
 
-    // Adds a new hacker to the inventory
-    suspend fun addUnusedHacker() {
-        val inventoryData = inventoryDataFlow.first()
-        var unusedHackers = inventoryData.unusedHackers
-        gameDao.updateUnusedHackers(++unusedHackers)
+    // Adds a new lvl 1 hacker to the inventory
+    suspend fun addNewHacker() {
+        gameDao.addNewHacker()
     }
 
-    // Activates a single unused hacker
-    suspend fun activateHacker() {
-        val inventoryData = inventoryDataFlow.first()
-        var activeHackers = inventoryData.activeHackers
-        var unusedHackers = inventoryData.unusedHackers
-        if (unusedHackers > 0) {
-            gameDao.updateHackers(++activeHackers, --unusedHackers)
+    // Uses a level k upgrade on a level k-1 hacker, if both exist
+    suspend fun upgradeHacker(upgradeLvl: Int) {
+        val inventory = inventoryDataFlow.first()
+        val hLvl1 = inventory.hackersLvl1
+        val hLvl2 = inventory.hackersLvl2
+        val hLvl3 = inventory.hackersLvl3
+        val hLvl4 = inventory.hackersLvl4
+        val hLvl5 = inventory.hackersLvl5
+
+        when (upgradeLvl) {
+            1 -> {
+                val upgrades = inventory.upgradeLvl2
+                if (hLvl1 > 0 && upgrades > 0) {
+                    gameDao.setHackers(hLvl1 - 1, hLvl2 + 1, hLvl3, hLvl4, hLvl5)
+                }
+            }
+            2 -> {
+                val upgrades = inventory.upgradeLvl3
+                if (hLvl2 > 0 && upgrades > 0) {
+                    gameDao.setHackers(hLvl1, hLvl2 - 1, hLvl3 + 1, hLvl4, hLvl5)
+                }
+            }
+            3 -> {
+                val upgrades = inventory.upgradeLvl4
+                if (hLvl3 > 0 && upgrades > 0) {
+                    gameDao.setHackers(hLvl1, hLvl2, hLvl3 - 1, hLvl4 + 1, hLvl5)
+                }
+            }
+            4 -> {
+                val upgrades = inventory.upgradeLvl5
+                if (hLvl4 > 0 && upgrades > 0) {
+                    gameDao.setHackers(hLvl1, hLvl2, hLvl3, hLvl4 - 1, hLvl5 + 1)
+                }
+            }
+            else -> {
+                println("Invalid upgrade level: $upgradeLvl")
+            }
         }
     }
 
-    // Adds a new crypto miner to the inventory
-    suspend fun addUnusedCryptoMiner() {
-        val inventoryData = inventoryDataFlow.first()
-        var unusedCryptoMiners = inventoryData.unusedCryptoMiners
-        gameDao.updateUnusedHackers(++unusedCryptoMiners)
-    }
+    // Uses a level k upgrade on a level k-1 crypto miner, if both exist
+    suspend fun upgradeCryptoMiner(upgradeLvl: Int) {
+        val inventory = inventoryDataFlow.first()
+        val cmLvl1 = inventory.cryptoMinersLvl1
+        val cmLvl2 = inventory.cryptoMinersLvl2
+        val cmLvl3 = inventory.cryptoMinersLvl3
+        val cmLvl4 = inventory.cryptoMinersLvl4
+        val cmLvl5 = inventory.cryptoMinersLvl5
 
-    // Activates a single unused crypto miner
-    suspend fun activateCryptoMiner() {
-        val inventoryData = inventoryDataFlow.first()
-        var activeCryptoMiners = inventoryData.activeCryptoMiners
-        var unusedCryptoMiners = inventoryData.unusedCryptoMiners
-        if (unusedCryptoMiners > 0) {
-            gameDao.updateHackers(++activeCryptoMiners, --unusedCryptoMiners)
+        when (upgradeLvl) {
+            1 -> {
+                val upgrades = inventory.upgradeLvl2
+                if (cmLvl1 > 0 && upgrades > 0) {
+                    gameDao.setHackers(cmLvl1 - 1, cmLvl2 + 1, cmLvl3, cmLvl4, cmLvl5)
+                }
+            }
+            2 -> {
+                val upgrades = inventory.upgradeLvl3
+                if (cmLvl2 > 0 && upgrades > 0) {
+                    gameDao.setHackers(cmLvl1, cmLvl2 - 1, cmLvl3 + 1, cmLvl4, cmLvl5)
+                }
+            }
+            3 -> {
+                val upgrades = inventory.upgradeLvl4
+                if (cmLvl3 > 0 && upgrades > 0) {
+                    gameDao.setHackers(cmLvl1, cmLvl2, cmLvl3 - 1, cmLvl4 + 1, cmLvl5)
+                }
+            }
+            4 -> {
+                val upgrades = inventory.upgradeLvl5
+                if (cmLvl4 > 0 && upgrades > 0) {
+                    gameDao.setHackers(cmLvl1, cmLvl2, cmLvl3, cmLvl4 - 1, cmLvl5 + 1)
+                }
+            }
+            else -> {
+                println("Invalid upgrade level: $upgradeLvl")
+            }
         }
     }
 
-    // Adds a new botnet to the inventory
-    suspend fun addUnusedBotnet() {
-        val inventoryData = inventoryDataFlow.first()
-        var unusedBotnets = inventoryData.unusedBotnets
-        gameDao.updateUnusedHackers(++unusedBotnets)
+    // Uses a level k upgrade on a level k-1 botnet, if both exist
+    suspend fun upgradeBotnet(upgradeLvl: Int) {
+        val inventory = inventoryDataFlow.first()
+        val bLvl1 = inventory.botnetsLvl1
+        val bLvl2 = inventory.botnetsLvl2
+        val bLvl3 = inventory.botnetsLvl3
+        val bLvl4 = inventory.botnetsLvl4
+        val bLvl5 = inventory.botnetsLvl5
+
+        when (upgradeLvl) {
+            1 -> {
+                val upgrades = inventory.upgradeLvl2
+                if (bLvl1 > 0 && upgrades > 0) {
+                    gameDao.setHackers(bLvl1 - 1, bLvl2 + 1, bLvl3, bLvl4, bLvl5)
+                }
+            }
+            2 -> {
+                val upgrades = inventory.upgradeLvl3
+                if (bLvl2 > 0 && upgrades > 0) {
+                    gameDao.setHackers(bLvl1, bLvl2 - 1, bLvl3 + 1, bLvl4, bLvl5)
+                }
+            }
+            3 -> {
+                val upgrades = inventory.upgradeLvl4
+                if (bLvl3 > 0 && upgrades > 0) {
+                    gameDao.setHackers(bLvl1, bLvl2, bLvl3 - 1, bLvl4 + 1, bLvl5)
+                }
+            }
+            4 -> {
+                val upgrades = inventory.upgradeLvl5
+                if (bLvl4 > 0 && upgrades > 0) {
+                    gameDao.setHackers(bLvl1, bLvl2, bLvl3, bLvl4 - 1, bLvl5 + 1)
+                }
+            }
+            else -> {
+                println("Invalid upgrade level: $upgradeLvl")
+            }
+        }
     }
 
-    // Activates a single unused botnet
-    suspend fun activateBotnet() {
-        val inventoryData = inventoryDataFlow.first()
-        var activeBotnets = inventoryData.activeBotnets
-        var unusedBotnets = inventoryData.unusedBotnets
-        if (unusedBotnets > 0) {
-            gameDao.updateHackers(++activeBotnets, --unusedBotnets)
-        }
+    // Adds a new lvl 1 crypto miner to the inventory
+    suspend fun addNewCryptoMiner() {
+        gameDao.addNewCryptoMiner()
+    }
+
+    // Adds a new lvl 1 botnet to the inventory
+    suspend fun addNewBotnet() {
+        gameDao.addNewBotnet()
     }
 
     suspend fun addUpgradeLvl2() {
