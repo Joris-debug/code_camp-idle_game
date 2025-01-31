929daabe621d3afd6b8155673ddbd96131fa2671
Alex <alex.khr@yahoo.com>
2025-01-31 01:43:29 +0100

    Fixed bugs.

--- Idle_Game/app/src/main/java/com/example/idle_game/ui/views/models/StartViewModel.kt
+++ Idle_Game/app/src/main/java/com/example/idle_game/ui/views/models/StartViewModel.kt
@@ -3,9 +3,6 @@ package com.example.idle_game.ui.views.models
 import android.Manifest
 import android.app.Activity
 import android.content.pm.PackageManager
-import android.os.Build
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.platform.LocalContext
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModel
