7fe0e1ffa1c314b880b1e2a1c28eafdaa5f9e96b
Alex <alex.khr@yahoo.com>
2025-01-31 12:52:51 +0100

    Fixed bugs.

--- Idle_Game/app/src/main/java/com/example/idle_game/ui/views/composable/BluetoothSubViews.kt
+++ Idle_Game/app/src/main/java/com/example/idle_game/ui/views/composable/BluetoothSubViews.kt
@@ -1,7 +1,6 @@
 package com.example.idle_game.ui.views.composable
 
 import android.annotation.SuppressLint
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
